e12efc6addb15a671190023d27e46a36dfb1f4f2
Oladele Marwan <oladelemarwan@gmail.com>
2022-04-13 16:03:00 +0100

    new commit

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,13 +1,18 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
-  return <main>
-    <button className='sidebar-toggle'>
-      <FaBars/>
-    </button>
-    <button className='btn'> show modal</button>
-  </main>
-}
+  const data = useGlobalContext();
+  console.log(data);
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn"> show modal</button>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
