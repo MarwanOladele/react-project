7e736d8a7a021d0124ecf409c49b8af9b8692c68
Oladele Marwan <oladelemarwan@gmail.com>
2022-04-18 13:19:21 +0100

    strip submenu completed

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,51 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
 
-export default Submenu
+  const container = useRef(null);
+
+  const [column, setColumn] = useState('col-2')
+
+  useEffect(() => {
+    setColumn('col-2')
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+
+    if(links.length === 3) {
+setColumn('col-3')
+    }
+     if (links.length > 3) {
+       setColumn("col-4");
+     }
+
+  }, [location, links]);
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${column}`}>
+        {links.map((link, i) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url} key={i}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
