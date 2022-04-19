3b43e7d3e671c100f04c555f878f0bd0364b7bc1
Oladele Marwan <oladelemarwan@gmail.com>
2022-04-19 14:41:27 +0100

    cart project complted

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -24,12 +24,37 @@ const AppProvider = ({ children }) => {
     dispatch({ type: "REMOVE", payload: id });
   };
 
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
-        remove
+        remove,
+        decrease,
+        increase,
       }}
     >
       {children}
