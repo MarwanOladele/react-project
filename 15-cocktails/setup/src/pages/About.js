a534052ea5cc0fcec0fed348aed8c6798aa6e19c
Oladele Marwan <oladelemarwan@gmail.com>
2022-04-22 18:47:06 +0100

    new commit

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+   <section className='section about-section'>
+     <h1 className='section-title'>About</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis numquam dolorem, architecto alias, mollitia illo magnam expedita dolor illum dignissimos nisi a sint eaque ipsa eum iusto sequi eius. Unde non esse quidem vitae, id porro eos! Magnam itaque deserunt dolorum, cupiditate, corporis eos, reiciendis iste ipsam suscipit maxime distinctio.</p>
+   </section>
   )
 }
 
